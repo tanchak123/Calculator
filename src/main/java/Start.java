26a4111b4867744cef0799366fc622ba6992ca2f
Andrey Tanchak <tanchak12@gmail.com>
2020-07-02 19:05:30 +0300

    set notResizable window

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -9,6 +9,7 @@ public class Start {
         this.window = new JFrame("Calculator");
         window.setSize(336, 500);
         window.setLocationRelativeTo(null);
+        window.setResizable(false);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.add(new Panel());
         window.setVisible(true);
